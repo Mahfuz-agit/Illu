68f75fd806aae8f19ac4fff49c4de2b7ae31f800
Mahfuz-agit <rnlnit@protonmail.com>
2026-08-28 17:31:52 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,9 @@ class ParentalControlApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Parental Control',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'TicTacToe Classic',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
       home: const ModeSelectorScreen(),
     );
   }
@@ -25,23 +26,21 @@ class ModeSelectorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Select Device Mode")),
+      appBar: AppBar(title: const Text("Setup Mode")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton.icon(
-              icon: const Icon(Icons.admin_panel_settings, size: 30),
-              label: const Text("I am the Parent", style: TextStyle(fontSize: 20)),
-              style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
-              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => ParentScreen())),
+              icon: const Icon(Icons.admin_panel_settings),
+              label: const Text("Parent Control Mode"),
+              onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => ParentScreen())),
             ),
-            const SizedBox(height: 40),
+            const SizedBox(height: 20),
             ElevatedButton.icon(
-              icon: const Icon(Icons.child_care, size: 30),
-              label: const Text("I am the Child", style: TextStyle(fontSize: 20)),
-              style: ElevatedButton.styleFrom(padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
-              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => ChildScreen())),
+              icon: const Icon(Icons.sports_esports),
+              label: const Text("Start Child Mode (Game)"),
+              onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => ChildScreen())),
             ),
           ],
         ),
